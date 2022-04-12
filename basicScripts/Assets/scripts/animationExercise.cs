2ad46737fbe54c8830ac4f4e6f5a6a1e3e151492
designerasun <asuntheawesome@gmail.com>
2022-04-12 15:19:41 +0900

    feat: understanding how animation works in unity

---
+++ basicScripts/Assets/scripts/animationExercise.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationExercise : MonoBehaviour
+{
+    // character sometimes needs to be interactive(often interact with other objects), which requires animation
+    // the hierachy of game character: 
+    // 1) object with animator component 
+    // 2) mesh(render character)  
+    // 3) character skeleton(rig) 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
